632eb38aa8093885845aa1de9a2d278d2c791785
Aravindh Kumar Mari <aravindhkumar.mari@bounteous.com>
2025-02-19 13:49:01 +0530

    [GenCode_1739953141.045293] Auto Code change

---
+++ code.java
@@ -0,0 +1,19 @@
+ between Oracle and MS SQL
+Oracle Connection to MS SQL:
+
+Here is an example of how you can connect to Microsoft SQL using Oracle SQL Developer:
+
+1. Open SQL Developer and open a new database.
+2. Click on the "Database" button on the top-left corner.
+3. Select "MS SQL" from the list of databases.
+4. Click on the "New Connection" button.
+5. Enter your login information and select a username for the connection.
+6. Select a password for the connection.
+7. Enter your database server information.
+8. Select a database to connect to.
+9. Select a schema to connect to.
+10. Click on the "Test Connection" button.
+11. If the connection test is successful, you can click on the "Close" button.
+12. You can now use SQL queries to connect to MS SQL using Oracle SQL.
+
+Here is an example of how you
